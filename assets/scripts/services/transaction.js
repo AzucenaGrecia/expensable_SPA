4b6870d5d5d8cf6e0b825fe419b63cb3085ce927
Grecia Delgado <greciadm2596@gmail.com>
2021-02-07 17:17:13 -0500

    Add sytles into form componet

--- assets/scripts/services/transaction.js
+++ assets/scripts/services/transaction.js
@@ -1,3 +1,21 @@
 import { BASE_URL, apiFetch } from "./api_fetch.js";
 
-export function listTra
+export function listTransactions() {
+  return apiFetch(`${BASE_URL}/categories/${categoryId}/transactions`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Token token=${sessionStorage.getItem('token')}`
+    }
+  })
+}
+
+export function createTransaction(categoryId) {
+  return apiFetch(`${BASE_URL}/categories/${categoryId}/transactions`, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token token=${sessionStorage.getItem('token')}`
+    },
+    body: JSON.stringify(amount, note, date)
+  })
+}
